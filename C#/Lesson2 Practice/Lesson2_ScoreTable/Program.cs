f5732b14132ae1419d7283919768a9a0c8746063
Exo4R <yuribiron@gmail.com>
2022-10-03 19:11:16 +0300

    Lesson2 Practice C# addes

---
+++ C#/Lesson2 Practice/Lesson2_ScoreTable/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string fullName = "Иванов Иван Иванович";
+        int age = 16;
+        string email = "ivanov@mail.ru";
+        double programmingScore = 4.7;
+        double mathsScore = 5;
+        double physicsScore = 4.2;
+
+        Console.WriteLine($"ФИО: {fullName}");
+        Console.WriteLine($"Возраст: {age}");
+        Console.WriteLine($"E-mail: {email}");
+        Console.WriteLine($"Баллы по программированию: {programmingScore}");
+        Console.WriteLine($"Баллы по математике: {mathsScore}");
+        Console.WriteLine($"Баллы по физике: {physicsScore}");
+        Console.ReadKey(true);
+
+        // Подсчет суммы баллов
+
+        double totalAmount = programmingScore + mathsScore + physicsScore;
+        Console.WriteLine($"Сумма баллов: {totalAmount}");
+        Console.ReadKey(true);
+        double avgScore = (totalAmount) / 3;
+        Console.WriteLine($"Средний балл: {avgScore.ToString("#.##")}");
+        Console.ReadKey(true);
+    }
+}
---
+++ C#/Lesson2 Practice/Lesson2_ScoreTable/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Lesson2_ScoreTable")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lesson2_ScoreTable")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("06102e7a-a4e3-42c5-be5a-13c776f1f79d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
